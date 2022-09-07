dbf53ac26176f698fefbc3654952293080e74968
Sebastian <dustypaws@gmail.com>
2022-09-07 20:58:21 +0200

    Fix: Show hover window when cursor is on a line that shows an error.

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -3,26 +3,30 @@ local settings = require('user')
 
 -- Close nvim if NvimTree is only running buffer
 api.nvim_create_autocmd(
-  "BufEnter",
-  { command = [[if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]] }
+    "BufEnter",
+    { command = [[if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif]] }
 )
 
 -- Format buffer on save
 api.nvim_create_autocmd(
     "BufWritePre", {
-        pattern = "*",
-        command = [[lua vim.lsp.buf.formatting_sync()]]
-    }
+    pattern = "*",
+    command = [[lua vim.lsp.buf.formatting_sync()]]
+}
 )
 
 if settings.packer_auto_sync then
-  -- source plugins.lua and run PackerSync on save
-  local sync_packer = function()
-    vim.cmd("runtime lua/plugins.lua")
-    require("packer").sync()
-  end
-  api.nvim_create_autocmd({ "BufWritePost" }, {
-    pattern = { "plugins.lua" },
-    callback = sync_packer,
-  })
+    -- source plugins.lua and run PackerSync on save
+    local sync_packer = function()
+        vim.cmd("runtime lua/plugins.lua")
+        require("packer").sync()
+    end
+    api.nvim_create_autocmd({ "BufWritePost" }, {
+        pattern = { "plugins.lua" },
+        callback = sync_packer,
+    })
 end
+
+vim.cmd([[
+autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false })
+]])
--- lua/config/lsp-zero.lua
+++ lua/config/lsp-zero.lua
@@ -1,5 +1,20 @@
 local lsp = require('lsp-zero')
 
+lsp.set_preferences({
+    suggest_lsp_servers = true,
+    setup_servers_on_start = true,
+    set_lsp_keymaps = true,
+    configure_diagnostics = true,
+    cmp_capabilities = true,
+    manage_nvim_cmp = true,
+    call_servers = 'local',
+    sign_icons = {
+        error = '✘',
+        warn = '▲',
+        hint = '⚑',
+        info = ''
+    }
+})
 local rust_lsp = lsp.build_options('rust_analyzer', {})
 lsp.configure('rust_analyzer', {
     on_attach = function(c, bufnr) 
@@ -8,7 +23,9 @@ lsp.configure('rust_analyzer', {
     end
 })
 
-lsp.preset('recommended')
+lsp.nvim_workspace()
 lsp.setup()
 
 require('rust-tools').setup({server = rust_lsp})
+require("mason").setup()
+require("mason-lspconfig").setup()
