07f3f8e8f1e3bc76207d9aced9c37cc270b3d4f4
Sebastian <dustypaws@gmail.com>
2024-08-14 15:50:09 +0200

    Updates and fixins

--- lua/config/treesitter.lua
+++ lua/config/treesitter.lua
@@ -1,5 +1,14 @@
+vim.filetype.add({
+    extension = {
+        c3 = "c3",
+        c3i = "c3",
+        c3t = "c3",
+    },
+})
+
+
 require('nvim-treesitter').setup {
-  ensure_installed = { "lua", "rust", "toml" },
+  ensure_installed = { "lua", "rust", "toml", "c3" },
   auto_install = true,
   highlight = {
     enable = true,
@@ -11,6 +20,16 @@ require('nvim-treesitter').setup {
     extended_mode = true,
     max_file_lines = nil,
   }
+
+}
+
+local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+parser_config.c3 = {
+    install_info = {
+        url = "https://github.com/c3lang/tree-sitter-c3",
+        files = {"src/parser.c", "src/scanner.c"},
+        branch = "main",
+    },
 }
 
 -- Treesitter folding 
